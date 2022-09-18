92a82bea0a36800a6aac7a9afa48687e899f7d04
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-09-18 15:52:32 -0300

    nabu: doesn't use GSI Keys and making another variable

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,13 +30,7 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 
-ifeq ($(NABU_KERNEL),true)
-BOARD_BOOT_HEADER_VERSION := 3
-#KERNEL_LD := LD=ld.lld
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
-TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu-kernel
-TARGET_KERNEL_CONFIG := nabu_user_defconfig
-else ($(NABU_KERNEL_PREBUILT),true)
+ifeq ($(NABU_KERNEL_PREBUILT),true)
 #vendor_cmdline
 VENDOR_CMDLINE := "console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm androidboot.init_fatal_reboot_target=recovery androidboot.selinux=permissive androidboot.vbmeta.avb_version=1.0 androidboot.boot_devices=soc/1d84000.ufshc"
 #Kernel Variables
@@ -51,6 +45,14 @@ BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE) --board ""
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/$(PRODUCT_RELEASE_NAME)/dtb.img
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/$(PRODUCT_RELEASE_NAME)/dtbo.img
+endif
+
+ifeq ($(NABU_KERNEL),true)
+BOARD_BOOT_HEADER_VERSION := 3
+#KERNEL_LD := LD=ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu-kernel
+TARGET_KERNEL_CONFIG := nabu_user_defconfig
 else
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150-legacy
 TARGET_KERNEL_CONFIG += vendor/xiaomi/nabu.config
